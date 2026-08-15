449d7f4297291022a2b97f46ec7c2d069b33875f
nwekekent <nwekep14@gmail.com>
2026-08-15 13:14:09 +0100

    feat: add exploration navigation drawer

---
+++ src/data/explorations.ts
@@ -0,0 +1,14 @@
+export const explorations = [
+  {
+    title: "Button With Options",
+    path: "/button-with-options/",
+  },
+  {
+    title: "File Upload Block",
+    path: "/file-upload-block/",
+  },
+  {
+    title: "Pricing Card",
+    path: "/interactive-pricing-card/",
+  },
+] as const;
